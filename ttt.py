c2f6b8e8e013958ad8c349c8930c569df15e7656
Michael Jeffrey <5944277+michaeljeffrey@users.noreply.github.com>
2020-09-10 20:54:30 -0700

    reworked draw_board function

--- ttt.py
+++ ttt.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
-def draw_board(board_map):
-    print(f" {board_map[0][0]} | {board_map[0][1]} | {board_map[0][2]}\n - - - - -\n {board_map[1][0]} | {board_map[1][1]} | {board_map[1][2]}\n - - - - -\n {board_map[2][0]} | {board_map[2][1]} | {board_map[2][2]}")
-    return
-
 """
+next:
+accept valid inputs
+check for win
+clear screen ?
+AI for computer turn
+"""
+
+def draw_board(board_map):
+    '''draw the TTT board'''
+    counter = 0
+    row = 0
+    while counter < 5:
+        if counter % 2 == 0:
+            print(f" {board_map[row][0]} | {board_map[row][1]} | {board_map[row][2]}")
+            counter += 1
+            row += 1
+        else:
+            print(f"- - - - -")
+            counter += 1
+    return
+"""
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
  1 | 2 | 3 
  - - - - -
  4 | 5 | 6
@@ -12,33 +30,16 @@ def draw_board(board_map):
  7 | 8 | 9
 """
 
-def myfunc():
-    # row = " {num1} | {num2} | {num3}"
-    # line = "- - - - -"
-    counter = 0
-    while counter < 5:
-        if counter % 2 == 0:
-            num = 4
-            print(f" {num} | {num} | {num}")
-            counter += 1
-        else:
-            print(f"- - - - -")
-            counter += 1
-
-
 def choose_position(board_map, human_turn):
+    '''choose a position on the board either by human input or AI'''
     if human_turn:
         position = input ("Enter position number: ")
-        # print(position)
         valid = False
         for row in range(3):
             for col in range(3):
-                # print(row, col)
-                # print(board_map[row][col])
                 if int(position) == board_map[row][col]:
                     board_map[row][col] = 'X'
                     valid = True
-        # print(board_map)
 
         # for row in board_map:
         #     print(row)
@@ -49,15 +50,15 @@ def choose_position(board_map, human_turn):
     return
 
 def ttt():
+    ''' main '''
     board_map =  [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    draw_board(board_map)
     game_over = False
-    myfunc()
+    draw_board(board_map)
     while not game_over:
         human_turn = True
         choose_position(board_map, human_turn)
         draw_board(board_map)
-
+    return
 
 if __name__ == "__main__":
     ttt()
